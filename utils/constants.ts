fe5701aad226530921764ccac4f70b5fc62ae38d
void-ness <78547746+void-ness@users.noreply.github.com>
2022-12-20 03:01:24 +0530

    update: programm id

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,2 +1,2 @@
-export const STUDENT_INTRO_PROGRAM_ID = '5nnkcSwxYim5KYXLVobAyXkAkJnoNEEa3xqfiRqcnyts'
+export const STUDENT_INTRO_PROGRAM_ID = '74924nh8hecnZepNGeewEHyUXm4qGnh31ULU857V2Bfd'
 // export const STUDENT_INTRO_PROGRAM_ID = '92HR2ghpZJFKt6XdQfGW5ZMyLuEXjZo1YBrq49x1w1nV'
